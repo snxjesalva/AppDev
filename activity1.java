f1cd01525b215944d379fb93ad663755fda07a8f
JohnMarkBalais <jpbalais@paterostechnologicalcollege.edu.ph>
2025-02-18 19:34:50 +0800

    Add files via upload

--- activity1.java
+++ activity1.java
@@ -1,7 +1,9 @@
 public class StudentNames {
     public static void main(String[] args) {
-        System.out.println("Heart Janelle Serrano - BSIT 2D");
+        System.out.println("John Mark P. Balais - BSIT 2D");
         // Students, add your names below using the format:
-        System.out.println("Gabrielle Daniela P. Miranda - BSIT 2C");
+        // System.out.println("Your Name - SECTION");
+        // Example:
+        // System.out.println("Juam Dela Cruz - BSIT 2C");
     }
 }
