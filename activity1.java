a018872051ad649aac19e0c40ab140ae18e42a5c
unknown <hdserrano@paterostechnologicalcollege.edu.ph>
2025-02-18 19:25:56 +0800

    Added my name Heart Janelle

--- activity1.java
+++ activity1.java
@@ -1,6 +1,6 @@
 public class StudentNames {
     public static void main(String[] args) {
-        System.out.println("List of Students:");
+        System.out.println("Heart Janelle Serrano - BSIT 2D");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
