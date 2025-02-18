1317d5a87af380f25d819f86e215e28248a273fb
Bendoy, Jomar R. <jrbendoy@paterostechnologicalcollege.edu.ph>
2025-02-18 20:17:55 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -1,7 +1,6 @@
 public class StudentNames {
     public static void main(String[] args) {
         System.out.println("John Mark P. Balais - BSIT 2D");
-        System.out.println("Jomar R. Bendoy - BSIT 2D");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
