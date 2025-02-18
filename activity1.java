02d0a46a4c6d273b8e3ffb8a1be4a974799b1f6e
Heart Janelle Serrano <hdserrano@paterostechnologicalcollege.edu.ph>
2025-02-19 01:31:03 +0800

    activity1.java

--- activity1.java
+++ activity1.java
@@ -2,6 +2,7 @@ public class StudentNames {
     public static void main(String[] args) {
         System.out.println("Sandara L. Panis - BSIT 2D");
         System.out.println("Eisen Dichoso - BSIT 2D");
+         System.out.println("Heart Janelle D. Serrano - BSIT 2D");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
