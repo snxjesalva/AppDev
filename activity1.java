fe607cf530b5e0111815f93ef9316efe3051bd66
EisenDichoso <eadichoso@paterostehnologicalcollege.edu.ph>
2025-02-19 00:12:23 +0800

    added my name Eisen Dichoso

--- activity1.java
+++ activity1.java
@@ -1,6 +1,7 @@
 public class StudentNames {
     public static void main(String[] args) {
         System.out.println("Sandara L. Panis - BSIT 2D");
+        System.out.println("Eisen Dichoso - BSIT 2D");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
