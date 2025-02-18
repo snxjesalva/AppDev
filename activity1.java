42c6665425219476ff93750af0a481da08879d75
lemonjoes <jvalabado@paterostechnologicalcollege.edu.ph>
2025-02-18 19:01:26 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -1,6 +1,8 @@
 public class StudentNames {
     public static void main(String[] args) {
         System.out.println("List of Students:");
+
+        System.out.println("Juffer Victor Alabado - BSIT 2D");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
