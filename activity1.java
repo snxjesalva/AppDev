9dc52f2ef540ee5d7e425d7f3a81c2c031a70a54
arawgabie <gpmiranda@paterostechnologicalcollege.edu.ph>
2025-02-18 18:53:06 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -2,8 +2,6 @@ public class StudentNames {
     public static void main(String[] args) {
         System.out.println("List of Students:");
         // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Juam Dela Cruz - BSIT 2C");
+        System.out.println("Gabrielle Daniela P. Miranda - BSIT 2C");
     }
 }
