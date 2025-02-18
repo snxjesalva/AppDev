de18c186442cfe388ab525783a8719b1e45c8780
Markgelo0527 <mlmayo@paterostechnologicalcollege.edu.ph>
2025-02-18 19:52:34 +0800

    Update Activity1.java

--- Activity1.java
+++ Activity1.java
@@ -1,10 +1,1 @@
-public class StudentNames {
-    public static void main(String[] args) {
-        System.out.println("List of Students:");
-        System.out.println("Mark Angelou L. Mayo - BSIT 2D");
-        // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Eavenheart Monteclaro - BSIT 2C");
-    }
-}
+
