ea6add0a246315d42feac2e74f777ecb39d89a91
Ryan Echon <rpechon@paterostechnologicalcollege.edu.ph>
2025-02-18 20:43:55 +0800

    activity1.java

--- activity1.java
+++ activity1.java
@@ -2,6 +2,7 @@ public class StudentNames {
     public static void main(String[] args) {
         System.out.println("John Mark P. Balais - BSIT 2D");
         System.out.println("Jomar R. Bendoy - BSIT 2D");
+	System.out.println("Ryan Paul P. Echon Jr. - BSIT2C");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
