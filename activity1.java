71387b382a6e6b489c37788501074d9af8256322
shanemico <sjacal@paterostechnologicalcollege.edu.ph>
2025-02-19 23:54:28 +0800

    Activity1.java

--- activity1.java
+++ activity1.java
@@ -1,10 +1,6 @@
 public class StudentNames {
     public static void main(String[] args) {
-        System.out.println("Sandara L. Panis - BSIT 2D");
-        System.out.println("Eisen Dichoso - BSIT 2D");
-        System.out.println("Heart Janelle D. Serrano - BSIT 2D");
-        System.out.println("Gabrielle Daniela P. Miranda - BSIT 2C");
-        System.out.println("Leonardo M. Sayas - BSIT 2D");
+        System.out.println("Shane Mico J. Acal - BSIT 2C");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
