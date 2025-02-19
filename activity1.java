6237210faa0598e6e7fd01806fcb7f7a12b4f33b
Ralph Jastine Villon <rvvillon@paterostechnologicalcollege.edu.ph>
2025-02-19 14:14:48 +0800

    Added name: Ralph Jastine Villon

--- activity1.java
+++ activity1.java
@@ -2,6 +2,7 @@ public class StudentNames {
     public static void main(String[] args) {
         System.out.println("List of Students:");
 
-        // System.out.println(" Ralph Jastine Villon - BSIT 2C");
+        // System.out.println(" Ralph Jastine V. Villon - BSIT 2C");
+
     }
 }
