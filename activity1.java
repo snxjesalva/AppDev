6537d6cc742f169b0b1261fe8182d97dda663a3e
Tyrone James Casuga <tyronejamescasuga@gmail.com>
2025-02-22 14:38:16 -0900

    Added name: Tyrone James Casuga

--- activity1.java
+++ activity1.java
@@ -5,6 +5,7 @@ public class StudentNames {
         System.out.println("Heart Janelle D. Serrano - BSIT 2D");
         System.out.println("Gabrielle Daniela P. Miranda - BSIT 2C");
         System.out.println("Leonardo M. Sayas - BSIT 2D");
+        System.out.println("Tyrone James Casuga - BSIT 2C");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
