6fd4ff7b688c9df21355372aae86a91064b2e423
BSIT2CBerte <jmberte@paterostechnologicalcollege.edu.ph>
2025-03-01 22:27:58 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -1,6 +1,6 @@
 public class StudentNames {
     public static void main(String[] args) {
-        System.out.println("List of Students:");
+        System.out.println("John Michael Berte - BSIT 2C");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
